693abdf18148ddb953ea35be96f864d488387105
binubims <binubimshara2015@gmail.com>
2025-04-01 18:58:06 -0700

    querySelector is just like getElementById. It allows us to grab single HTML element from the HTML file. This can be used to make changes to our HTML file using js

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 let titleElement = document.getElementById("title");
 console.log(titleElement);
+
+let paragraph = document.querySelector(".para");
+console.log(paragraph);
+
+let yesOrNo = document.querySelector("ul");
+console.log(yesOrNo);
+
+let link = document.querySelector("#link")
+console.log(link);
